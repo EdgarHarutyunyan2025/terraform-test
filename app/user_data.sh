f93e291c931b4246ff3f2fb10b83ebe47c637e68
edgar_harutyunyan_2025 <edgar_harutyunyan_2025@mail.ru>
2025-03-24 12:02:30 +0400

    update web server

--- app/user_data.sh
+++ app/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum -y update
 yum -y install httpd
-echo "<h2>GitHub   Action ! </h2><br>Build by GitHub Action !" > /var/www/html/index.html
+echo "<h2>Terraform AWS ! </h2><br>Build by GitHub Action !" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
