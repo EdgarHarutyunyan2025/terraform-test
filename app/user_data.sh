37a838a7a92eef3f33da2a88aabcb15445f63725
edgar_harutyunyan_2025 <edgar_harutyunyan_2025@mail.ru>
2025-03-24 11:54:45 +0400

    add new line

--- app/user_data.sh
+++ app/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum -y update
 yum -y install httpd
-echo "<h2>Hello  Terraform ! </h2><br>Build by Teeraform !" > /var/www/html/index.html
+echo "<h2>GitHub   Action ! </h2><br>Build by Teeraform !" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
